b3e73d30f616957a75d4aaa7729e5e129d3a3019
al3ztm <alexbm2hd@gmail.com>
2025-07-08 20:02:18 -0500

    cambie algo

---
+++ matenme.js
@@ -0,0 +1,12 @@
+
+function saludo(Nombre){
+alert("hola " + Nombre)
+}
+
+saludo("Nombre")
+saludo("Juan")
+saludo("Enano")
+saludo("loquilla")
+saludo("tetas")
+saludo("teturras")
+saludo("mamalungas")
